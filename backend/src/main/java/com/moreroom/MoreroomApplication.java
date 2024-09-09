40e80d39432929719572d1436c5d1f887121d67f
박민준 <ppmm98@naver.com>
2024-09-09 11:06:58 +0900

    be/chore: backend project setting
    백엔드 기본 프로젝트 세팅
    - 백엔드 프로젝트 세팅

---
+++ backend/src/main/java/com/moreroom/MoreroomApplication.java
@@ -0,0 +1,13 @@
+package com.moreroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoreroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoreroomApplication.class, args);
+	}
+
+}
