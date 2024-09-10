8e8e7c18a10cfc7a8acc17d28bba1158b14199d1
박민준 <ppmm98@naver.com>
2024-09-10 09:08:34 +0900

    be/feat: Region Entity
    지역 엔티티 생성
    - 멤버 정보에 쓸 Region Entity 임시 생성

---
+++ backend/src/main/java/com/moreroom/domain/region/entity/Region.java
@@ -0,0 +1,27 @@
+package com.moreroom.domain.region.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@ToString(callSuper = true)
+public class Region {
+    @Id
+    @Column(nullable = false, length = 50)
+    private String regionId;
+    @Column(nullable = false, length = 100)
+    private String regionName;
+    @Column(nullable = false)
+    @Builder.Default
+    private Integer regionLevel = 2;
+}
