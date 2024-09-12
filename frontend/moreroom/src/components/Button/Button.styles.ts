51999356bd4cb56e4f277fbe463a412ee398d875
조선미 <tjs2194@naver.com>
2024-09-12 10:20:05 +0900

    fe/feat: Button active, disabled 색 설정 완료
    Button active, disabled 색 설정 완료
    - active 색은 A100
    - disabled 색은 200
    
    #9

--- frontend/moreroom/src/components/Button/Button.styles.ts
+++ frontend/moreroom/src/components/Button/Button.styles.ts
@@ -31,12 +31,35 @@ export const variantCss = (variant: ButtonVariant, color: Palette, scale?: Color
         background-color: ${!scale ? MainColors[color] : Colors[color][scale]};
         color: white;
         border : 2px solid ${!scale ? MainColors[color] : Colors[color][scale]};
+
+        :enabled {
+          :active {
+            background-color: ${Colors[color]['A100']};
+            border: 2px solid ${Colors[color]['A100']};
+          }
+        }
+
+        :disabled {
+          background-color: ${Colors[color]['200']};
+          border: 2px solid ${Colors[color]['200']};
+        }
       `;
     case 'outlined':
       return css`
         background-color: transparent;
         color: ${!scale ? MainColors[color] : Colors[color][scale]};
         border: 2px solid ${!scale ? MainColors[color] : Colors[color][scale]};
+
+        :enabled {
+          :active {
+            border: 2px solid ${Colors[color]['A100']};
+          }
+        }
+
+        :disabled {
+          color: 2px solid ${Colors[color]['200']};;
+          border: 2px solid ${Colors[color]['200']};
+        }
       `;
     default:
       return css``;
