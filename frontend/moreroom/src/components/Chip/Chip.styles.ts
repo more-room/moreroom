9e4cdc1ea20cc1e92f3140ef8191987e6339d7fe
조선미 <tjs2194@naver.com>
2024-09-19 13:49:47 +0900

    fe/feat: Chip 코드 수정
    코드 수정
    - padding 수정
    
    #33

--- frontend/moreroom/src/components/Chip/Chip.styles.ts
+++ frontend/moreroom/src/components/Chip/Chip.styles.ts
@@ -1,5 +1,4 @@
 import { css } from '@emotion/react';
-import { Colors } from '../../styles/globalStyle';
 import { FontWeight, Palette } from '../../types/globalStyleTypes';
 import ColorStyle from '../../styles/colorStyle';
 
@@ -9,7 +8,8 @@ export const base = (color:Palette, border:number, fontSize:number, fontWeight:F
   color: ${ColorStyle.text[color]};
   box-shadow: 0 0 0.125rem 0 #ccc;
   user-select: none;
-  padding: 0.375rem 0.625rem;
+  padding: 0.5rem 1rem;
   font-size: ${fontSize}rem;
   font-weight: ${fontWeight};
+  text-align: center;
 `;
