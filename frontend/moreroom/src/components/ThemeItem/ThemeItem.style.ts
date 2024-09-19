c7380f30647768bab463cfc0779345f1336a61d1
조선미 <tjs2194@naver.com>
2024-09-20 01:46:40 +0900

    fe/feat: themeItem 구현 (ing)
    코드
    - css 수정 필요(padding)
    - div -> 타이포그래피로 바꾸기
    
    #39

--- frontend/moreroom/src/components/ThemeItem/ThemeItem.style.ts
+++ frontend/moreroom/src/components/ThemeItem/ThemeItem.style.ts
@@ -1,5 +1,35 @@
 import { css } from '@emotion/react';
+import { Colors } from '../../styles/globalStyle';
 
 export const base = css`
   background-color: #313131;
+  display: flex;
+  align-items: center;
+  padding: 0.375rem;
 `;
+
+export const imgCss = css`
+  width: 5rem;
+  height: 6.25rem;
+`
+
+export const brandCss = css`
+  color: ${Colors['grey']['500']};
+  font-size: 0.625rem;
+  font-weight: 600;
+`
+
+export const titleCss = css`
+  color:#fff;
+  font-size: 1rem;
+  font-weight: 700;
+  padding-left: 0.55rem;
+`
+
+export const contentCss = css`
+  color: ${Colors['grey']['500']};
+  font-size: 0.76rem;
+  font-weight: 400;
+  padding-top: 0.375rem;
+  padding-left: 0.55rem;
+`
--- frontend/moreroom/src/components/ThemeItem/index.tsx
+++ frontend/moreroom/src/components/ThemeItem/index.tsx
@@ -1,21 +1,46 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react';
 import { ThemeItemInfoProps } from './ThemeItem.types';
-import { base } from './ThemeItem.style';
+import { base, brandCss, contentCss, imgCss, titleCss } from './ThemeItem.style';
+import { MapPinIcon, StarIcon } from '@heroicons/react/24/solid';
+import { Icon } from '../Icon';
 
 export const ThemeItemInfo = ({
   poster,
   title,
-  genreList,
+  genreList = ['기타'],
   brandName,
   branchName,
   playtime,
   review,
   ...props
-}:ThemeItemInfoProps) => {
+}: ThemeItemInfoProps) => {
   return (
     <div css={base} {...props}>
-      
+      <div >
+        <img css = {imgCss} src={`${process.env.PUBLIC_URL}/2ways.jpg`} alt="포스터 사진" />
+      </div>
+      <div>
+        <div css={base}>
+          <Icon color="primary" size={1}>
+            <MapPinIcon />
+          </Icon>
+
+          <div css={brandCss}>
+            {brandName} - {branchName}
+          </div>
+        </div>
+        <div css={[titleCss]}>{title}</div>
+        <div css={[contentCss]}>
+          {playtime} / {genreList.join(', ')}
+        </div>
+        <div css = {[base, contentCss]}>
+        <Icon color="secondary" size={1}>
+          <StarIcon />
+        </Icon>
+        <div>리뷰({review})</div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
