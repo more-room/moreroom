29b03b1fbf7f30719d09c9ad32318fd0b4bfa90a
NANEUNSIK <skadmstlrek@naver.com>
2024-09-20 11:18:34 +0900

    fe/feat: InfoBox 아이콘, 텍스트 크기 조절 해야함 커밋
    #40

--- frontend/moreroom/src/components/InfoBox/index.tsx
+++ frontend/moreroom/src/components/InfoBox/index.tsx
@@ -1,22 +1,34 @@
 /** @jsxImportSource @emotion/react */
-import React, {Children} from 'react';
+import React from 'react';
 import { base } from './InfoBox.styles';
 import { InfoBoxProps } from './InfoBox.types';
-import { number } from 'prop-types';
+import { ClockIcon  } from '@heroicons/react/24/solid';
+import { Icon } from '../Icon';
+import { Colors, MainColors } from '../../styles/globalStyle';
 
 export const InfoBox = ({
   color = 'primary',
-  fontSize = number,
+  backgroundColor = 'grey', 
+  fontSize = 1.5,
   fontWeight = 400,
-  scale,
-  size = 1,
+  colorScale,
+  backgroundColorScale,
+  size = 1.5,
   borderRadius = 0.5,
   children = 'text',
   ...props
 }: InfoBoxProps) => {
+  const iconColor = colorScale ? Colors[color][colorScale] : MainColors[color];  // 아이콘 색상
+  const iconSize = fontSize * 1.2 + 'rem';  // 아이콘 크기 설정 (텍스트 크기와 연동)
+
   return (
-    <span css={base( color, scale, size, fontSize, fontWeight, borderRadius)} {...props}>
-      {children}
-    </span>
-  )
+    <div css={base( color, backgroundColor, size, borderRadius, fontWeight, fontSize, colorScale, backgroundColorScale )} {...props}>
+      <div className='text'>{children}</div>
+      <div className='icon'>
+        <Icon>
+          <ClockIcon  style={{ color: iconColor, width: iconSize, height: iconSize }} />  {/* 아이콘 색상 및 크기 적용 */}
+        </Icon>
+      </div>
+    </div>
+  );
 }
