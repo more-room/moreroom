c2209e52d93dc8de4c56dc2b56de4f1eb4578a0b
박민준 <ppmm98@naver.com>
2024-09-20 13:14:14 +0900

    be/feat: 파티 요청 외래 키 도입

--- backend/src/main/java/com/moreroom/domain/partyRequest/entity/PartyRequest.java
+++ backend/src/main/java/com/moreroom/domain/partyRequest/entity/PartyRequest.java
@@ -2,14 +2,19 @@ package com.moreroom.domain.partyRequest.entity;
 
 import static com.moreroom.domain.partyRequest.entity.MatchingStatus.NOT_MATCHED;
 
+import com.moreroom.domain.member.entity.Member;
+import com.moreroom.domain.theme.entity.Theme;
 import com.moreroom.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -28,10 +33,12 @@ public class PartyRequest extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long partyRequestId;
-    @Column(nullable = false)
-    private Long memberId;
-    @Column(nullable = false)
-    private Integer themeId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "memberId")
+    private Member member;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "themeId")
+    private Theme theme;
     private LocalDateTime lastMatchedAt;
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
