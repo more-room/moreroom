fbf49a11fb15bf1c2f899d24c9dd9e75c66b9b23
최요하 <yoha6865@gmail.com>
2024-09-20 16:19:28 +0900

    fe/feat: 테마 제목 검색 페이지
    테마의 제목만 검색하는 페이지 구현
     - theme mocking 적용
    
    #50

--- frontend/moreroom/src/types/themeTypes.ts
+++ frontend/moreroom/src/types/themeTypes.ts
@@ -1,41 +1,65 @@
-import { ReactNode } from "react";
-
 export interface ITheme {
-  themeId?: number,
-  poster?: string,
-  title?: string,
-  genreId?: number,
-  genrename?: string,
-  playtime?: number,
-  minPeople?: number,
-  maxPeople?: number,
-  level?: number,
-  price?: number,
-  description?: string,
-  memberLevel?: number,
-  reviewCount?: number,
-  reviewScore?: number,
-  brandName?: string,
-  branchName?: string,
-  playFlag?: boolean,
+  themeId?: number;
+  poster?: string;
+  title?: string;
+  genreId?: number;
+  genrename?: string;
+  playtime?: number;
+  minPeople?: number;
+  maxPeople?: number;
+  level?: number;
+  price?: number;
+  description?: string;
+  memberLevel?: number;
+  reviewCount?: number;
+  reviewScore?: number;
+  brandName?: string;
+  branchName?: string;
+  playFlag?: boolean;
   // cafeId: number,
   // regionId: string,
   // address: string,
 }
 
 export interface ThemeList {
-  themeList : ITheme[];
+  themeList: ITheme[];
 }
 
 export interface ICafe {
-  cafeId: number,
-  brandId: number,
-  regionId: string,
-  address: string,
-  cafeName: string,
-  tel: string,
-  link: string,
-  latitude: number,
-  longitude: number,
-  themeCount: number
+  cafeId: number;
+  brandId: number;
+  regionId: string;
+  address: string;
+  cafeName: string;
+  tel: string;
+  link: string;
+  latitude: number;
+  longitude: number;
+  themeCount: number;
+}
+
+/* 제목 검색 결과 */
+export interface ISearchTitleResult {
+  themeId: number;
+  title: string;
+}
+
+/* 제목 검색 결과 리스트 */
+export interface ISearchTitleResponse {
+  themeList: ISearchTitleResult[];
+}
+
+/* 테마 검색 요청 */
+export interface ISearchThemesRequestParameter {
+  genreList: string[];
+  people: number;
+  region: string;
+  level: number;
+  brandId: number;
+  playtime: number;
+  price: number;
+  title: string;
+  pageNumber: number;
+  pageSize: number;
+  sort: string;
 }
