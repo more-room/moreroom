b9fcce7647b2da7c2ff81845649eaeeea959e551
박민준 <ppmm98@naver.com>
2024-09-20 13:14:21 +0900

    be/feat: 리뷰 외래 키 도입

--- backend/src/main/java/com/moreroom/domain/review/entity/Review.java
+++ backend/src/main/java/com/moreroom/domain/review/entity/Review.java
@@ -1,11 +1,16 @@
 package com.moreroom.domain.review.entity;
 
+import com.moreroom.domain.member.entity.Member;
+import com.moreroom.domain.theme.entity.Theme;
 import com.moreroom.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,10 +30,12 @@ public class Review extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long reviewId;
-    @Column(nullable = false)
-    private Long memberId;
-    @Column(nullable = false)
-    private Integer themeId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "memberId", nullable = false)
+    private Member member;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "themeId", nullable = false)
+    private Theme theme;
     private String content;
     @Column(nullable = false)
     private float score;
