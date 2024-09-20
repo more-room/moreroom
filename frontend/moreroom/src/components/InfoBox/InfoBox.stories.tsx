3f96705fabd017f2c28b0678ca57d8e76a4c69e1
NANEUNSIK <skadmstlrek@naver.com>
2024-09-20 16:06:11 +0900

    fe/feat: InfoBox 컴포넌트 완료 커밋
    #40

--- frontend/moreroom/src/components/InfoBox/InfoBox.stories.tsx
+++ frontend/moreroom/src/components/InfoBox/InfoBox.stories.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { InfoBox } from '.';
 import { ColorScale, Palette } from '../../types/globalStyleTypes';
+import { ClockIcon, UserIcon } from '@heroicons/react/24/solid';
 
 const meta = {
   title: 'UI/Components/InfoBox',
@@ -19,17 +20,21 @@ const meta = {
   tags: ['!autodocs'],
   argTypes: {
     children: {
-      description: '아이콘을 넣어주세요',
+      description: '텍스트를 입력해주세요',
     },
     color: {
       description: '적용할 컬러입니다.\n기본 색상은 primary 입니다.',
     },
-    scale: {
+    colorScale: {
       description:
         '적용할 컬러스케일입니다.\n입력되지 않으면 MainColor가 적용됩니다.',
     },
     size: {
       description: '적용할 사이즈입니다.\nrem 단위로 입력하면 됩니다.',
+    },
+    icon: {
+      description: '사용할 아이콘의 이름을 입력하세요. 예: ClockIcon, UserIcon 등',
+      control: { type: 'text' },  // 텍스트 입력 가능
     },
   },
 } satisfies Meta<typeof InfoBox>;
@@ -38,10 +43,95 @@ export default meta;
 
 type Story = StoryObj<typeof InfoBox>;
 
+const colors: Palette[] = [
+  'primary',
+  'secondary',
+  'danger',
+  'grey',
+  'dark',
+  'light',
+];
+
+const scales: ColorScale[] = [
+  '50',
+  '100',
+  '200',
+  '300',
+  '400',
+  '500',
+  '600',
+  '700',
+  '800',
+  '900',
+  'A100',
+  'A200',
+  'A400',
+  'A700',
+];
+
+const icons: ReactNode[] = [<ClockIcon/>, <UserIcon/>]
+
 export const Primary: Story = {
   args: {
-    children: 'text',
+    children: '09월 08일15:00',
     color: 'primary',
-    size: 1,
+    size: 1.5,
+    fontSize: 1,
+    borderRadius: 0.8,
+    icon: <ClockIcon/>
   },
 };
+
+export const MainColorsInfoBox: Story = {
+  args: {
+    ...Primary.args,
+  },
+  render: (args) => (
+    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '1rem' }}>
+      {colors.map((color) => (
+        <InfoBox key={color} {...args} color={color}>
+          {color}
+        </InfoBox>
+      ))}
+    </div>
+  ),
+};
+
+export const ColorsWithScales: Story = {
+  args: {
+    ...Primary.args,
+  },
+  render: (args) => (
+    <div>
+      {colors.map((color) => (
+        <div
+          key={color}
+          style={{ display: 'flex', flexWrap: 'wrap', gap: '1rem' }}
+        >
+          {color != 'dark' &&
+            color != 'light' &&
+            scales.map((scale) => (
+              <InfoBox key={scale} style={{ marginBottom: '1rem' }} {...args} color={color} colorScale={scale}>
+                {scale}
+              </InfoBox>
+            ))}
+        </div>
+      ))}
+    </div>
+  ),
+};
+
+export const InfoBoxIcon: Story = {
+  args: {
+    ...Primary.args,
+  },
+  render: (args) => (
+    <div style={{ display: 'flex', flexWrap: 'wrap', gap: '1rem' }}>
+      {icons.map((icon) => (
+        <InfoBox key={icon?.toString()} {...args} icon={icon}>
+          
+        </InfoBox>
+      ))}
+    </div>
+  ),
+}
--- frontend/moreroom/src/components/InfoBox/index.tsx
+++ frontend/moreroom/src/components/InfoBox/index.tsx
@@ -2,31 +2,34 @@
 import React from 'react';
 import { base } from './InfoBox.styles';
 import { InfoBoxProps } from './InfoBox.types';
-import { ClockIcon  } from '@heroicons/react/24/solid';
+import { ClockIcon, UserIcon } from '@heroicons/react/24/solid';
 import { Icon } from '../Icon';
 import { Colors, MainColors } from '../../styles/globalStyle';
 
+
+
 export const InfoBox = ({
   color = 'primary',
-  backgroundColor = 'grey', 
-  fontSize = 1.5,
+  icon = <ClockIcon/>,  //아이콘을 붙여넣기 가능하게 
+  fontSize = 1,
   fontWeight = 400,
   colorScale,
-  backgroundColorScale,
   size = 1.5,
-  borderRadius = 0.5,
-  children = 'text',
+  borderRadius = 0.8,
+  children = '09월08일15:00',
   ...props
 }: InfoBoxProps) => {
-  const iconColor = colorScale ? Colors[color][colorScale] : MainColors[color];  // 아이콘 색상
+  // const iconColor = colorScale ? Colors[color][colorScale] : MainColors[color];  // 아이콘 색상
   const iconSize = fontSize * 1.2 + 'rem';  // 아이콘 크기 설정 (텍스트 크기와 연동)
 
+
   return (
-    <div css={base( color, backgroundColor, size, borderRadius, fontWeight, fontSize, colorScale, backgroundColorScale )} {...props}>
+    <div css={base( color, size, borderRadius, fontWeight, fontSize, colorScale, )} {...props}>
       <div className='text'>{children}</div>
       <div className='icon'>
-        <Icon>
-          <ClockIcon  style={{ color: iconColor, width: iconSize, height: iconSize }} />  {/* 아이콘 색상 및 크기 적용 */}
+        <Icon size={fontSize * 1.2} color='grey'>
+          {/* <ClockIcon  style={{ color: Colors['grey']['500'], width: iconSize, height: iconSize }} />   */}
+          {icon}
         </Icon>
       </div>
     </div>
