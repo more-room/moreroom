7f503e4b7dcdcd70b721947c9be13344450aa1cb
NANEUNSIK <skadmstlrek@naver.com>
2024-09-22 17:42:23 +0900

    fe/feat: ThemeRecommend 90% 완료 커밋
    #52

--- frontend/moreroom/src/components/ThemeRecommend/ThemeRecommend.styles.ts
+++ frontend/moreroom/src/components/ThemeRecommend/ThemeRecommend.styles.ts
@@ -4,23 +4,32 @@ import { Colors } from '../../styles/globalStyle';
 export const containerCss = css`
   background-color: #313131;
   display: flex;
-  height: 6.25rem;
+  width: 20rem;
+  height: 20.25rem;
   align-items: flex-start;
   border-radius: 0.25rem;
   margin-bottom: 10rem;
 `;
 
 export const imgCss = css`
-  width: 5rem;
-  height: 6.25rem;
+  width: 18rem;
+  height: 13.25rem;
   border-radius: 0.25rem;
+  margin-left: 0.5rem;      
+  margin-right: 1rem; 
+  margin-top: 0.25rem;
+  margin-bottom: 0.25rem;
+  position: 'relative';
 `;
+
+
 
 export const infoCss = css`
   padding: 0.375rem;
   display: flex;
   flex-direction: column;
   gap: 0.375rem;
+  padding-left: 0.7rem;
 `;
 
 export const infoItemCss = css`
@@ -34,8 +43,42 @@ export const contentCss = css`
 `;
 
 export const lineCss = css`
-  width: 70vw; // 스크린 크기의 70%
-  border: 0.0625rem solid ${Colors['grey']['900']};
+  width: 50vw; // 스크린 크기의 70%
+  border: 0.0625rem solid ${Colors['light']['900']};
   margin-top: 0.18rem !important;
   align-self: flex-end;
+`;
+
+export const checkboxContainerCss = css`
+  width: 1rem;  // 박스 크기
+  height: 1rem;
+  background-color: ${Colors['primary']['A200']}; // 배경색 설정
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.1rem;  // 둥근 모서리
+`;
+
+// 체크 아이콘 스타일
+export const checkIconCss = css`
+  color: ${Colors['dark']};  // 체크 아이콘 색상
+  width: 1.5rem;
+  height: 1.5rem;
+  
+`;
+
+export const titleCss = css`
+  padding-left: 0.5rem;
+`
+
+export const imgWrapperCss = css`
+  position: relative; /* 자식들이 이 요소를 기준으로 배치됨 */
+  width: fit-content; /* 이미지 크기와 동일하게 설정 */
+`;
+export const ratingCss = css`
+  position: absolute;
+  bottom: 6.3rem;
+  left: 0.5rem;
+  padding: 0.25rem 0.5rem;
+  
 `;
--- frontend/moreroom/src/components/ThemeRecommend/index.tsx
+++ frontend/moreroom/src/components/ThemeRecommend/index.tsx
@@ -8,35 +8,39 @@ import {
   infoCss,
   infoItemCss,
   lineCss,
+  checkboxContainerCss,
+  checkIconCss,
+  titleCss,
+  ratingCss,
+  imgWrapperCss
+
 } from './ThemeRecommend.styles';
-import { MapPinIcon, StarIcon } from '@heroicons/react/24/solid';
+import { StarIcon, CheckIcon } from '@heroicons/react/24/solid';
 import { Icon } from '../Icon';
+import { ExpectedRating } from '../ExpectedRating';
 import { Typography } from '../Typography';
 import { LabeledTypography } from '../LabeledTypography';
 
 export const ThemeRecommend = ({
   theme,
-  pattern='',
+  pattern = '',
   ...props
 }: ThemeRecommendProps) => {
   return (
     <div css={containerCss} {...props}>
-      <div >
-        <img
-          css={imgCss}
-          src={theme?.poster}
-          alt="포스터 사진"
-        />
-      </div>
-      <div css={infoCss}>
-        <div css={infoItemCss}>
-          <Icon color="primary" size={1}>
-            <MapPinIcon />
-          </Icon>
-          <Typography color="grey" scale="500" size={0.625} weight={600}>
-            {theme?.brandName} - {theme?.branchName}
-          </Typography>
+      {/* 포스터 출력 */}
+       <div css={imgWrapperCss}>
+        <div css={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+          <img css={imgCss} src={theme?.poster} alt="포스터 사진" />
+
+          {/* 예상 등급 (포스터 왼쪽 하단에 배치) */}
+          <div css={ratingCss}>
+            <ExpectedRating size={0.5}/>
+          </div>
         </div>
+          
+          {/* 테마 제목 */}
+        <div css={titleCss}>
           <LabeledTypography
             normalColor="light"
             pattern={pattern}
@@ -44,24 +48,44 @@ export const ThemeRecommend = ({
             str={theme?.title ?? '테마 없음'} // undefined일 경우 '테마 없음' 출력
             weight={700}
           />
-        <Typography
-          css={contentCss}
-          color="grey"
-          scale="500"
-          size={0.76}
-          weight={400}
-        >
-          {theme?.playtime}분 / {theme?.genrename}
-        </Typography>
-        <div css={infoItemCss}>
-          <Icon color="secondary" size={1}>
-            <StarIcon />
-          </Icon>
-          <Typography color="grey" scale="500" size={0.76} weight={400}>
-            리뷰({theme?.reviewCount})
-          </Typography>
         </div>
-        <div css={lineCss}></div>
+
+        {/* 포스터 밑에 정보 출력 */}
+        <div css={infoCss}>
+          {/* 위치 정보 */}
+          <div css={infoItemCss}>
+            <Typography color="grey" scale="500" size={0.625} weight={600}>
+              {theme?.brandName} - {theme?.branchName}
+            </Typography>
+          </div>
+          
+          {/* 사용자 성향에 맞춘 추천 항목 */}
+          <div css={infoItemCss}>
+            <Icon color="dark" size={1}>
+              <div css={checkboxContainerCss}>
+                <CheckIcon css={checkIconCss} /> 
+              </div>
+            </Icon>
+            <Typography color="light" scale="500" size={0.76} weight={400}>
+               나와 비슷한 성향의 사용자가 좋아하는 테마
+              <div css={lineCss}></div>
+            </Typography>
+          </div>
+          
+
+          {/* 테마와 유사한 높은 별점 테마 */}
+          <div css={infoItemCss}>
+            <Icon color="dark" size={1}>
+              <div css={checkboxContainerCss}>
+                <CheckIcon css={checkIconCss} /> 
+              </div>
+            </Icon>
+            <Typography color="light" scale="500" size={0.76} weight={400}>
+               높은 별점을 주신 'Bad Rob Bad'와 비슷한 테마
+              <div css={lineCss}></div>
+            </Typography>
+          </div>
+        </div>
       </div>
     </div>
   );
