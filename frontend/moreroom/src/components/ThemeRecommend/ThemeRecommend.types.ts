7c0bdb22f4e5bcb22d91bb09728a0ba361c9237c
NANEUNSIK <skadmstlrek@naver.com>
2024-09-23 09:44:30 +0900

    fe/feat: ThemeRecommend  theme? 수정 커밋
    #52

--- frontend/moreroom/src/components/ThemeRecommend/ThemeRecommend.types.ts
+++ frontend/moreroom/src/components/ThemeRecommend/ThemeRecommend.types.ts
@@ -2,6 +2,6 @@ import { ReactNode } from "react";
 import { IThemeListItem } from "../../types/themeTypes";
 
 export interface ThemeRecommendProps extends React.ComponentProps<'div'>  {
-  theme?: IThemeListItem,
+  theme: IThemeListItem,
   pattern?:string,
 }
