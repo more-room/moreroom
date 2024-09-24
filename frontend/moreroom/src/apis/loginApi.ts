3e6e409ad49901bda6a176e9d4aa32491344c4c3
최요하 <yoha6865@gmail.com>
2024-09-24 16:50:49 +0900

    fe/fix: loginApi endpoint 수정

--- frontend/moreroom/src/apis/loginApi.ts
+++ frontend/moreroom/src/apis/loginApi.ts
@@ -1,8 +1,7 @@
 import { api } from './interceptors';
-import  axios  from 'axios';
 
 export const UserLogin = (email: string, password: string) => {
-  return axios.post('http://j11d206.p.ssafy.io/api/auth/member/login', {
+  return api.post('/api/auth/member/login', {
     email: email,
     password: password,
   });
