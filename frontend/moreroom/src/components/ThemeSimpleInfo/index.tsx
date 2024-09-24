b65ba0ea34915d2372a18c688410d21110d5be4d
최요하 <yoha6865@gmail.com>
2024-09-24 10:45:32 +0900

    fe/fix: ThemeSimpleInfo 리팩토링
    컴포넌트 구조 수정
     - span, div -> typography 컴포넌트 활용
    
    스타일 -> emotion 적용
    
    #69

--- frontend/moreroom/src/components/ThemeSimpleInfo/index.tsx
+++ frontend/moreroom/src/components/ThemeSimpleInfo/index.tsx
@@ -1,18 +1,26 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react';
 import { ThemeSimpleInfoProps } from './ThemeSimpleInfo.types';
-import { base, genreCss, imgCss, titleCss } from './ThemeSimpleInfo.styles';
+import { base, imgCss } from './ThemeSimpleInfo.styles';
+import { Typography } from '../Typography';
 export const ThemeSimpleInfo = ({ theme, ...props }: ThemeSimpleInfoProps) => {
   return (
     <div css={base} {...props}>
       <img css={imgCss} src={theme.poster} alt="포스터 사진" />
-      <div css={titleCss}>{theme.title}</div>
-      <div css={genreCss}>
+      <Typography
+        color="light"
+        size={0.875}
+        weight={500}
+        style={{ whiteSpace: 'nowrap' }}
+      >
+        {theme.title}
+      </Typography>
+      <Typography color="grey" size={0.75} weight={400}>
         {theme.genreList.map(
           (genre: string, idx: number) =>
             genre + (idx === theme.genreList.length - 1 ? '' : ', '),
         )}
-      </div>
+      </Typography>
     </div>
   );
 };
