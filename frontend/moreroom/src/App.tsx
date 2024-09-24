c45c7ee634614a72c28359ee1f895d0d91a48220
최요하 <yoha6865@gmail.com>
2024-09-24 10:46:19 +0900

    fe/feat: 추천 테마 조회 페이지 구현
    추천 테마 3종류
    - 각각의 종류마다 테마 조회
    - mocking data
    
    파티 매칭 정보는 현재 미완성
    
    #69

--- frontend/moreroom/src/App.tsx
+++ frontend/moreroom/src/App.tsx
@@ -6,6 +6,7 @@ import { Modal } from './components/Modal';
 import { useModalStore } from './stores/modalStore';
 import { Login } from './pages/Login';
 import { Chating } from './pages/Chating';
+import { Main } from './pages/Main';
 
 function App() {
   const modalStore = useModalStore();
@@ -13,10 +14,10 @@ function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Main />} />
         <Route path="/themes" element={<ThemeList />} />
         <Route path="/login" element={<Login />} />
         {/* <Route path="/chating" element={<Chating />} /> */}
-        
       </Routes>
       {modalStore.isOpen && (
         <Modal height={modalStore.height}>{modalStore.contents}</Modal>
