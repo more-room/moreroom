4167dfd89a449eff04b007f9290a24a8a5809cd5
yr5698 <yr5698@gmail.com>
2024-09-25 10:13:31 +0900

    be/fix: 추천 서비스 로그 삭제

--- backend/src/main/java/com/moreroom/domain/recommendations/service/RecommendationService.java
+++ backend/src/main/java/com/moreroom/domain/recommendations/service/RecommendationService.java
@@ -48,7 +48,6 @@ public class RecommendationService {
             .orElseThrow(
                 HistoryNotFoundException::new);
         Integer themeId = history.getTheme().getThemeId();
-        System.out.println("lastVisitedThemeId = " + themeId);
         // 2. 유사 테마 조회 (mongoDB)
         SimilarTheme similarTheme = similarThemeRepository.findByThemeId(themeId);
         // 3. 테마 상세 정보 조회
