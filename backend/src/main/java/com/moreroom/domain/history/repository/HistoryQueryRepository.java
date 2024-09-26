60019a05189bc7b86b6cc716717ab1d28d848081
yr5698 <yr5698@gmail.com>
2024-09-26 17:46:10 +0900

    be/feat: 기록 목록 조회

---
+++ backend/src/main/java/com/moreroom/domain/history/repository/HistoryQueryRepository.java
@@ -0,0 +1,76 @@
+package com.moreroom.domain.history.repository;
+
+import static com.moreroom.domain.cafe.entity.QCafe.cafe;
+import static com.moreroom.domain.history.entity.QHistory.history;
+import static com.moreroom.domain.theme.entity.QTheme.theme;
+
+import com.moreroom.domain.cafe.entity.Cafe;
+import com.moreroom.domain.history.dto.HistoryListResponseDto;
+import com.moreroom.domain.history.dto.HistoryListResponseDto.HistoryListComponentDto;
+import com.moreroom.domain.history.dto.HistoryListResponseDto.HistoryListComponentDto.HistoryListThemeDto;
+import com.moreroom.domain.history.entity.History;
+import com.moreroom.domain.theme.entity.Theme;
+import com.moreroom.global.repository.QuerydslRepositoryCustom;
+import com.moreroom.global.util.StringUtil;
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HistoryQueryRepository extends QuerydslRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public HistoryQueryRepository(JPAQueryFactory jpaQueryFactory) {
+        super(theme);
+        this.jpaQueryFactory = jpaQueryFactory;
+    }
+
+    public HistoryListResponseDto findHistoriesByDate(Long memberId, LocalDateTime startDate,
+        LocalDateTime endDate) {
+        List<Tuple> results = jpaQueryFactory
+            .select(
+                history, theme, cafe
+            )
+            .from(history)
+            .leftJoin(theme).on(history.theme.themeId.eq(theme.themeId))
+            .leftJoin(cafe).on(theme.cafe.cafeId.eq(cafe.cafeId))
+            .where(
+                history.member.memberId.eq(memberId),
+                startDate == null ? null : history.playDate.after(startDate),
+                endDate == null ? null : history.playDate.before(endDate)
+            )
+            .fetch();
+
+        List<HistoryListComponentDto> list = new ArrayList<>();
+        for (Tuple t : results) {
+            History h = t.get(0, History.class);
+            Theme th = t.get(1, Theme.class);
+            Cafe c = t.get(2, Cafe.class);
+
+            assert th != null;
+            assert c != null;
+            list.add(HistoryListComponentDto.builder()
+                .historyId(h.getHistoryId())
+                .date(StringUtil.dateToString(h.getPlayDate()))
+                .memberPlayTime(h.getMemberPlayTime())
+                .hintCount(h.getHintCount())
+                .updatedAt(StringUtil.dateToString(h.getUpdatedAt()))
+                .theme(HistoryListThemeDto.builder()
+                    .themeId(th.getThemeId())
+                    .title(th.getTitle())
+                    .cafeName(c.getCafeName())
+                    .poster(th.getPoster())
+                    .build())
+                .build()
+            );
+        }
+
+        return HistoryListResponseDto.builder()
+            .historyList(list)
+            .build();
+    }
+}
--- backend/src/main/java/com/moreroom/global/util/StringUtil.java
+++ backend/src/main/java/com/moreroom/global/util/StringUtil.java
@@ -1,8 +1,37 @@
 package com.moreroom.global.util;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class StringUtil {
 
     public static boolean isEmpty(String str) {
         return str == null || str.trim().isEmpty();
+    }
+
+    public static String dateToString(LocalDateTime date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        String formattedDateTime = date.format(formatter);
+        return formattedDateTime;
+    }
+
+    public static LocalDateTime stringToDate(String date) {
+        if (date == null) {
+            return null;
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDateTime localDateTime = null;
+
+        try {
+            LocalDate localDate = LocalDate.parse(date, formatter);
+            localDateTime = localDate.atStartOfDay(); // 자정으로 설정
+        } catch (DateTimeParseException e) {
+            System.out.println("날짜 형식이 잘못되었습니다: " + e.getMessage());
+            return null;
+        }
+
+        return localDateTime;
     }
 }
