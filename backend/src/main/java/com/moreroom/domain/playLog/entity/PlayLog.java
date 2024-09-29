371d885916b9f449f40bbeb4e32681f38cf8baca
yr5698 <yr5698@gmail.com>
2024-09-30 00:33:58 +0900

    be/feat: history 생성, 삭제 시 playLog 함께 수정

--- backend/src/main/java/com/moreroom/domain/playLog/entity/PlayLog.java
+++ backend/src/main/java/com/moreroom/domain/playLog/entity/PlayLog.java
@@ -1,38 +1,39 @@
 package com.moreroom.domain.playLog.entity;
 
-import com.moreroom.domain.member.entity.Member;
-import com.moreroom.domain.theme.entity.Theme;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "playLog")
 @ToString(callSuper = true)
-@IdClass(PlayLog.class)
+@IdClass(PlayLogId.class)
 public class PlayLog {
     @Id
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "memberId")
-    private Member member;
-
+    private Long memberId;
     @Id
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "themeId")
-    private Theme theme;
+    private Integer themeId;
 
     private Integer playCount;
+
+
+    public void increasePlayCount() {
+        playCount++;
+    }
+
+    public void decreasePlayCount() {
+        playCount--;
+    }
 }
--- backend/src/main/java/com/moreroom/domain/playLog/repository/PlayLogRepository.java
+++ backend/src/main/java/com/moreroom/domain/playLog/repository/PlayLogRepository.java
@@ -2,8 +2,10 @@ package com.moreroom.domain.playLog.repository;
 
 import com.moreroom.domain.playLog.entity.PlayLog;
 import com.moreroom.domain.playLog.entity.PlayLogId;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlayLogRepository extends JpaRepository<PlayLog, PlayLogId> {
 
+    public Optional<PlayLog> findByMemberIdAndThemeId(Long memberId, Integer themeId);
 }
