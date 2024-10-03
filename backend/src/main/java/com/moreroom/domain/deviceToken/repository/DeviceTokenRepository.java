24ab7cd491d99a06d298f62c6e784a91455a379c
Elenaljh <hye9807@naver.com>
2024-10-03 15:50:00 +0900

    be/feat: 채팅 푸시 알림 기능 추가

--- backend/src/main/java/com/moreroom/domain/deviceToken/repository/DeviceTokenRepository.java
+++ backend/src/main/java/com/moreroom/domain/deviceToken/repository/DeviceTokenRepository.java
@@ -4,7 +4,11 @@ import com.moreroom.domain.deviceToken.entity.DeviceToken;
 import com.moreroom.domain.member.entity.Member;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DeviceTokenRepository extends JpaRepository<DeviceToken, Long> {
   Optional<DeviceToken> findByMember(Member member);
+
+  @Query("select dt from DeviceToken dt join Member m on dt.member = m where m.email = :email")
+  Optional<DeviceToken> findByEmail(String email);
 }
--- backend/src/main/java/com/moreroom/domain/party/controller/SocketController.java
+++ backend/src/main/java/com/moreroom/domain/party/controller/SocketController.java
@@ -1,5 +1,7 @@
 package com.moreroom.domain.party.controller;
 
+import com.moreroom.domain.deviceToken.dto.FcmMessageDto;
+import com.moreroom.domain.deviceToken.service.FcmService;
 import com.moreroom.domain.member.entity.Member;
 import com.moreroom.domain.party.dto.ChatMessageDto;
 import com.moreroom.domain.party.service.MessageService;
@@ -30,6 +32,7 @@ public class SocketController {
   private final FindMemberService findMemberService;
   private final PartyService partyService;
   private final MessageService messageService;
+  private final FcmService fcmService;
 
   @MessageMapping("/chat/join")
   public void joinChatroom(SocketNotificationDto dto, Principal principal) {
@@ -39,8 +42,11 @@ public class SocketController {
   }
 
   @MessageMapping("/chat/message")
-  public void sendMessage(ChatMessageDto message, Principal principal, MessageHeaders headers) {
-    messageService.saveMessage(message, principal);
-    simpMessagingTemplate.convertAndSend("/topic/party/" + message.getPartyId(), message, headers);
+  public void sendMessage(ChatMessageDto message, Principal principal, MessageHeaders headers, @Header("nickname") String nickname) {
+    messageService.saveMessage(message, principal); //메시지 저장
+    FcmMessageDto fcmMessageDto = fcmService.makeChattingPush(nickname, message.getMessage(),
+        principal.getName(), message.getPartyId());
+    fcmService.sendMessageTo(fcmMessageDto); //푸시알림 전송 -> 클라이언트에서는 소켓연결이 되지 않은 경우 소켓연결을 해야 함
+    simpMessagingTemplate.convertAndSend("/topic/party/" + message.getPartyId(), message, headers); //소켓메세지 전송
   }
 }
