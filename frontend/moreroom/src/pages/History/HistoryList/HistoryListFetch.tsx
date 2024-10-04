ebfd975c922928c2152ce4946a7631f9941d7b9c
최요하 <yoha6865@gmail.com>
2024-10-04 14:19:44 +0900

    fe/fix: 기록 목록이 있더라도 등록 버튼 추가
    #110

--- frontend/moreroom/src/pages/History/HistoryList/HistoryListFetch.tsx
+++ frontend/moreroom/src/pages/History/HistoryList/HistoryListFetch.tsx
@@ -82,6 +82,23 @@ export const HistoryListFetch = () => {
               onClick={() => nav(`/history/detail/${history.historyId}`)}
             />
           ))}
+          <Button
+            rounded={0.5}
+            handler={() => {
+              historyWriteStore.setDate(
+                calendarStore.selected
+                  ? calendarStore.selected.format('YYYY-MM-DD')
+                  : dayjs(
+                      `${calendarStore.curYear}-${calendarStore.curMonth}-01`,
+                    ).format('YYYY-MM-DD'),
+              );
+              nav('/history/write');
+            }}
+          >
+            <Typography color="light" weight={600} size={0.875}>
+              기록 등록하기
+            </Typography>
+          </Button>
         </div>
       ) : (
         <div css={empty}>
