e52313a7b8fa44efa1e74ac521dd85673e813445
yr5698 <yr5698@gmail.com>
2024-10-04 16:23:03 +0900

    be/feat: 외부 리뷰 키워드 중심 내용 노출되게 수정

--- backend/src/main/java/com/moreroom/domain/externalReview/dto/response/ExternalReviewResponseDto.java
+++ backend/src/main/java/com/moreroom/domain/externalReview/dto/response/ExternalReviewResponseDto.java
@@ -1,6 +1,7 @@
 package com.moreroom.domain.externalReview.dto.response;
 
 import com.moreroom.domain.externalReview.entity.ExternalReview;
+import com.moreroom.domain.externalReview.service.ExternalReviewExtractService;
 import com.moreroom.global.util.StringUtil;
 import lombok.Builder;
 import lombok.Data;
@@ -16,11 +17,11 @@ public class ExternalReviewResponseDto {
     private String createdAt;
     private Integer source;
 
-    public static ExternalReviewResponseDto toDto(ExternalReview er) {
+    public static ExternalReviewResponseDto toDto(ExternalReview er, String title) {
         return ExternalReviewResponseDto.builder()
             .externalReviewId(er.getExternalReviewId())
             .title(er.getTitle())
-            .content(er.getContent())
+            .content(ExternalReviewExtractService.getFirstKeyword(er.getContent(), title, null))
             .link(er.getLink())
             .createdAt(StringUtil.dateToString(er.getCreatedAt()))
             .source(er.getSource())
--- backend/src/main/java/com/moreroom/domain/externalReview/service/ExternalReviewService.java
+++ backend/src/main/java/com/moreroom/domain/externalReview/service/ExternalReviewService.java
@@ -3,6 +3,9 @@ package com.moreroom.domain.externalReview.service;
 import com.moreroom.domain.externalReview.dto.response.ExternalReviewResponseDto;
 import com.moreroom.domain.externalReview.entity.ExternalReview;
 import com.moreroom.domain.externalReview.repository.ExternalReviewRepository;
+import com.moreroom.domain.theme.entity.Theme;
+import com.moreroom.domain.theme.exception.ThemeNotFoundException;
+import com.moreroom.domain.theme.repository.ThemeRepository;
 import com.moreroom.global.dto.PageResponseDto;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -15,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 @AllArgsConstructor
 public class ExternalReviewService {
 
+    private final ThemeRepository themeRepository;
     private final ExternalReviewRepository externalReviewRepository;
 
     @Transactional(readOnly = true)
@@ -26,8 +30,9 @@ public class ExternalReviewService {
         }
         Page<ExternalReview> externalReviewPage = externalReviewRepository.findAllByThemeId(themeId,
             pageRequest);
+        Theme theme = themeRepository.findById(themeId).orElseThrow(ThemeNotFoundException::new);
         List<ExternalReviewResponseDto> externalReviewResponseDtoList = externalReviewPage.getContent()
-            .stream().map(ExternalReviewResponseDto::toDto).toList();
+            .stream().map(er -> ExternalReviewResponseDto.toDto(er, theme.getTitle())).toList();
         PageResponseDto pageResponseDto = PageResponseDto.builder()
             .pageNumber(externalReviewPage.getNumber())
             .pageSize(externalReviewPage.getSize())
