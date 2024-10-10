3984317574ac5f844d186c85ad3e48a81526c1ef
최요하 <yoha6865@gmail.com>
2024-10-10 12:55:22 +0900

    fe/fix: history 관련 api url 수정
    상대경로 -> 절대경로
    
    #155

--- frontend/moreroom/src/apis/historyApi.ts
+++ frontend/moreroom/src/apis/historyApi.ts
@@ -8,12 +8,12 @@ import { api } from './interceptors';
 
 /* 기록 목록 조회 */
 export const getHistoryList = (dateRange: IHistoryListRequestParameter) => {
-  return api.get<IHistoryList>('api/history', { params: dateRange });
+  return api.get<IHistoryList>('/api/history', { params: dateRange });
 };
 
 /* 기록 상세 조회 */
 export const getHistoryDetail = (historyId: number) => {
-  return api.get<IHistoryDetail>(`api/history/${historyId}`);
+  return api.get<IHistoryDetail>(`/api/history/${historyId}`);
 };
 
 /* 기록 등록 */
